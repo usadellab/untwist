aff4a6892149132de3c83f62fcf62b7b741dfded
Asis Hallab <asis.hallab@gmail.com>
2023-03-29 17:56:04 -0600

    Generated first draft of plot for paper

--- methods/admixture_plots.R
+++ methods/admixture_plots.R
@@ -1,3 +1,7 @@
+require(ggplot2)
+require(ggdendro)
+require(tidyr)
+
 #' Set working directory
 if (dir.exists("/mnt/data/asis/untwist/")) {
   setwd("/mnt/data/asis/untwist/")
@@ -66,10 +70,10 @@ for (k in admxtr_cv_errs[, "k"]) {
     ]
   #' Save the table:
   write.table(admxtr_tbl_k, paste0(
-      "./results/all_public_and_all_untwist_SNP_filtered_admixture_k",
-      k, "_result_table.tsv"
-    ),
-    sep = "\t", row.names=FALSE, quote=FALSE
+    "./results/all_public_and_all_untwist_SNP_filtered_admixture_k",
+    k, "_result_table.tsv"
+  ),
+  sep = "\t", row.names = FALSE, quote = FALSE
   )
 
   #' Plot
@@ -90,4 +94,42 @@ for (k in admxtr_cv_errs[, "k"]) {
     names.arg = admxtr_tbl_k$accession, cex.names = 0.5
   )
   dev.off()
+
+  #' Generate a circular tree plot with barplots attached to each leaf.
+  #' See
+  #' https://stackoverflow.com/questions/69227317/create-a-circular-phylogeny-with-stacked-bar-in-r
+  #' (last accessed 03/29/2023)
+  unt_tree <- dendro_data(unt_hclust)
+  admxtr_tibble_k <- pivot_longer(admxtr_tbl_k, paste0("V", 1:3))
+  scale <- 15
+  ax <- length(unique(admxtr_tibble_k$accession))
+  gg_plot <- ggplot() +
+    geom_col(
+      data = admxtr_tibble_k,
+      aes(
+        x = accession,
+        y = value, fill = factor(name)
+      )
+    ) +
+    geom_segment(
+      data = unt_tree$segments,
+      aes(x = x, y = -y * scale, xend = xend, yend = -yend * scale)
+    ) +
+    coord_polar() +
+    theme_minimal(axis.line=element_blank(),
+      axis.text.x=element_blank(),
+      axis.text.y=element_blank(),
+      axis.ticks=element_blank(),
+      axis.title.x=element_blank(),
+      axis.title.y=element_blank(),
+      legend.position="none",
+      panel.background=element_blank(),
+      panel.border=element_blank(),
+      panel.grid.major=element_blank(),
+      panel.grid.minor=element_blank(),
+      plot.background=element_blank()) +
+    #' theme_dendro() +
+    theme(
+      legend.position = "none"
+    )
 }
