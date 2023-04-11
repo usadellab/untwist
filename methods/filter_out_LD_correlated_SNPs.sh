945fd432a28bd7440cfe94a57e6b34a92e71f1f7
Asis Hallab <asis.hallab@gmail.com>
2023-04-11 07:09:52 +0200

    ALL results for LD-pruning window-size 1kb, max r^2 0.7

--- methods/filter_out_LD_correlated_SNPs.sh
+++ methods/filter_out_LD_correlated_SNPs.sh
@@ -17,7 +17,7 @@ echo "Hostname: ${HOSTNAME}"
 DIR=/mnt/data/asis/untwist
 cd ${DIR}
 
-/mnt/bin/bcftools/bcftools-1.9/bcftools +prune -l0.4 -Oz -o ./results/all_public_and_all_untwist_SNP_filtered.vcf.gz ./results/all_public_and_all_untwist_SNP_filtered_from_ata.vcf.gz;
+/mnt/bin/bcftools/bcftools-1.9/bcftools +prune -l0.7 -w 1000bp -Oz -o ./results/all_public_and_all_untwist_SNP_filtered.vcf.gz ./results/all_public_and_all_untwist_SNP_filtered_from_ata.vcf.gz;
 
 echo "**** Job ends ****"
 date
