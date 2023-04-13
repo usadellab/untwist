0d42cf8abefcb59d04f68ec9e26e277c8fd6ae09
Asis Hallab <asis.hallab@gmail.com>
2023-04-14 00:01:08 +0200

    Found another error in Ata's data
    - Ata used bcftools soft-filter, which markes low quality
      SNPs as such, but leaves them in the dataset.
    - Subsequent downstream analysis tools do not exclude SNps
      marked as low quality, rendering the filtering step
      idle.

--- methods/filter_out_LD_correlated_SNPs_SNPrelate.R
+++ methods/filter_out_LD_correlated_SNPs_SNPrelate.R
@@ -10,4 +10,12 @@ gds_fn <-  "./results/all_public_and_all_untwist_SNP_filtered.gds"
 snpgdsVCF2GDS(vcf_fn, gds_fn)
 
 genofile <- snpgdsOpen(gds_fn)
-snpset <- snpgdsLDpruning(genofile, ld.threshold=0.4)
+snpset <- snpgdsLDpruning(genofile, ld.threshold=0.4, autosome.only=FALSE)
+
+#' IBS Distance based clustering:
+snp_id <- unlist(unname(snpset))
+ibs.hc.f <- snpgdsHCluster(snpgdsIBS(genofile, num.thread=20,
+  snp.id=snp_id , autosome.only=FALSE))
+pdf('./results/SNPRelate_hclust_tree.pdf', width=21)
+plot(ibs.hc.f$dendrogram)
+dev.off()
